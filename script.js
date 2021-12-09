023e81b44f274075420131c8c57e6ae3669e8413
dobrynln <vladimirdobry3@gmail.com>
2021-12-09 14:21:33 +0800

    fix bags, add default value in localstorage

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 const result = document.querySelector('.calculating__result span');
 let sex, height, weight, age, ratio;
 
+if(localStorage.getItem('sex')) {
+    sex = localStorage.getItem('sex')
+} else {
+    sex = 'female'
+    localStorage.setItem('sex', 'female')
+}
+
+if(localStorage.getItem('ratio')) {
+    ratio = +localStorage.getItem('ratio')
+} else {
+    ratio = 1.375;
+    localStorage.setItem('ratio', 1.375)
+}
+
+
 function calcRes() {
     if(!sex || !height || !weight || !age || !ratio) {
         result.textContent = 'Не все поля заполнены.'
         return;
     }
     if(sex === 'female') {
-        result.textContent = Math.round(447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age));
+        result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
     } else {
-        result.textContent = Math.round(88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age));
+        result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
     }
 }
 calcRes();
+
+function initLocalStorage (parentSelector, activeClass) {
+    const elements = document.querySelectorAll(parentSelector);
+    elements.forEach( el => {
+        el.classList.remove(activeClass)
+        if(el.getAttribute('id') === localStorage.getItem('sex')) {
+            el.classList.add(activeClass)
+        }
+        if(el.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+            el.classList.add(activeClass)
+        }
+    })
+}
+initLocalStorage('#gender div', 'calculating__choose-item_active')
+initLocalStorage('.calculating__choose_big div', 'calculating__choose-item_active')
+
 function determineStaticInfo (parentSelector, activeClass) {
     const elements = document.querySelectorAll(parentSelector);
 
